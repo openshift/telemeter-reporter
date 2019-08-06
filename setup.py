cc35d6e252df0d7c346cb34d6a91c34e4b0f57c5
Anthony Byrne <abyrne@redhat.com>
2019-08-06 10:44:24 -0400

    Added HTML minification

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(name="telemeter-reporter", version="0.1.1", author="Anthony Byr
                  scripts=['bin/telemeter-reporter'], packages=setuptools.find_packages(),
                  install_requires=['PyYAML>=5.1.1', 'certifi>=2019.6.16', 'requests>=2.22.0',
                                    'PyJWT>=1.7.1', 'prometheus_api_client>=0.0.1',
-                                   'tabulate>=0.8.3', 'typing>=3.7.4'],
+                                   'tabulate>=0.8.3', 'typing>=3.7.4', 'htmlmin>=0.1.12'],
                  classifiers=["Programming Language :: Python :: 3.7",
                               "License :: OSI Approved :: Apache Software License",
                               "Operating System :: OS Independent",
